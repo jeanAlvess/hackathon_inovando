536b4eb4c8761825bcde571dc3a463501470200b
Jean Carlos Alves <jean-carlosalves@hotmail.com>
2020-11-07 23:32:20 -0300

    criando validaçoes final form

--- src/App.js
+++ src/App.js
@@ -4,15 +4,28 @@ import {
   Grid,
   Card,
   CardContent,
-  TextField,
   Box,
   Typography,
   InputAdornment,
   IconButton,
 } from '@material-ui/core/';
+
+import { Form } from 'react-final-form';
+import { TextField, makeValidate } from 'mui-rff';
 import { Visibility } from '@material-ui/icons';
+import * as Yup from 'yup';
+
+const schema = Yup.object().shape({
+  email: Yup.string().email().required(),
+  password: Yup.string().required(),
+});
 
 function App() {
+  function onSubmit(values) {
+    console.log(values);
+  }
+  const validate = makeValidate(schema);
+
   return (
     <Grid container justify="center">
       <Grid item md={4}>
@@ -22,35 +35,45 @@ function App() {
               <Typography variant="h4" align="center">
                 Login
               </Typography>
+              <Form
+                validate={validate}
+                onSubmit={onSubmit}
+                render={({ handleSubmit }) => (
+                  <form onSubmit={handleSubmit} noValidade>
+                    <TextField
+                      name="email"
+                      label="E-mail"
+                      type="email"
+                      variant="outlined"
+                      fullWidth
+                      margin="normal"
+                    />
+                    <TextField
+                      name="password"
+                      type="password"
+                      label="Password"
+                      variant="outlined"
+                      fullWidth
+                      margin="normal"
+                      InputProps={{
+                        endAdornment: (
+                          <InputAdornment>
+                            <IconButton>
+                              <Visibility />
+                            </IconButton>
+                          </InputAdornment>
+                        ),
+                      }}
+                    />
 
-              <TextField
-                label="E-mail"
-                type="email"
-                variant="outlined"
-                fullWidth
-                margin="normal"
+                    <Box display="flex" justifyContent="center" marginTop={4}>
+                      <Button variant="contained" color="primary" type="submit">
+                        Send
+                      </Button>
+                    </Box>
+                  </form>
+                )}
               />
-              <TextField
-                type="password"
-                label="Password"
-                variant="outlined"
-                fullWidth
-                margin="normal"
-                InputProps={{
-                  endAdornment: (
-                    <InputAdornment>
-                      <IconButton>
-                        <Visibility />
-                      </IconButton>
-                    </InputAdornment>
-                  ),
-                }}
-              />
-              <Box display="flex" justifyContent="center" marginTop={4}>
-                <Button variant="contained" color="primary">
-                  Send
-                </Button>
-              </Box>
             </Box>
           </CardContent>
         </Card>
